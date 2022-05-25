bf01dafdd0805668505348f95aa1c9afd0734bee
Maximilian Shupyro <suomi7862535@gmail.com>
2022-05-25 03:15:07 +0300

    demo gifs

--- gamestore/src/components/header/Header.jsx
+++ gamestore/src/components/header/Header.jsx
@@ -1,31 +1,32 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import './Header.css'
-import { ShoppingCartOutlined } from '@ant-design/icons'
-import { Badge, Popover } from 'antd'
-import { CartItem } from '../cart-item/CartItem'
-import { Button } from 'antd'
+import React from "react";
+import { useSelector } from "react-redux";
+import "./Header.css";
+import { ShoppingCartOutlined } from "@ant-design/icons";
+import { Badge, Popover } from "antd";
+import { CartItem } from "../cart-item/CartItem";
+import { Button } from "antd";
+import GamerHub from './assets/GamerHUB.png'
 
 const Header = () => {
-  const items = useSelector(state => state.cart.itemsInCart);
-  const totalPrice = items.reduce((acc, game) => acc += game.price , 0)
+  const items = useSelector((state) => state.cart.itemsInCart);
+  const totalPrice = items.reduce((acc, game) => (acc += game.price), 0);
 
   const content = (
-    <div className='cart-menu'>
-      <div className='cart-menu__games-list'>
+    <div className="cart-menu">
+      <div className="cart-menu__games-list">
         {items.length > 0
           ? items.map((game) => (
-            <CartItem
-              key={game.image}
-              image={game.image}
-              price={game.price}
-              title={game.name}
-							id={game.id}
-            />
-              ))
-            : "Your cart is empty"}
+              <CartItem
+                key={game.image}
+                image={game.image}
+                price={game.price}
+                title={game.name}
+                id={game.id}
+              />
+            ))
+          : "Your cart is empty"}
       </div>
-			{items.length > 0 ? (
+      {items.length > 0 ? (
         <div className="cart-menu__arrange">
           <div className="cart-menu__total-price">
             <span>Total sum:</span>
@@ -37,22 +38,24 @@ const Header = () => {
         </div>
       ) : null}
     </div>
-  )
+  );
 
   return (
-    <div className='header'>
-      <div className='header__left'></div>
-      <h1 className='header__title'>Gamer HUB</h1>
-      <div className='header__cart'>
+    <div className="header">
+      <div className="header__left"></div>
+      <img className="header__logo" src={GamerHub} />
+      <div className="header__cart">
         <Popover content={content}>
           <Badge count={items.length}>
-            <ShoppingCartOutlined style={{fontSize: '30px', color: '#FFFFFF'}}/>
+            <ShoppingCartOutlined
+              style={{ fontSize: "30px", color: "#FFFFFF" }}
+            />
           </Badge>
         </Popover>
-        <h2 className='header__total'>Total sum: {totalPrice}$</h2>
+        <h2 className="header__total">Total sum: {totalPrice}$</h2>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
